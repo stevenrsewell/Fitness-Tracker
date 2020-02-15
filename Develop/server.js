d13473b987b660b80ca2da9e9b32886a81023130
Steven Sewell <stevenrsewell@outlook.com>
2020-02-14 19:18:13 -0600

    server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
 const port = 3000
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-app.get('/api/workouts', function (req, res) {
-  res.send('yes')
-})
+// app.get('/api/workouts', function (req, res) {
+//   res.send('yes')
+// })
 
-app.post('/api/workouts', function (req, res) {
+// app.post('/api/workouts', function (req, res) {
 
-})
+// })
 
-app.put('/api/workouts', function (req, res) {
+// app.put('/api/workouts', function (req, res) {
 
-})
+// })
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
